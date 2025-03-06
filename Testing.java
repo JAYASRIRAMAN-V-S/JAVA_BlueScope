df0902537dd8950531ece94dc97c70d44c73c964
JAYASRIRAMAN-V-S <jayasriraman2003@example.com>
2025-03-06 22:48:48 +0530

    Commit03

--- Testing.java
+++ Testing.java
@@ -1,3 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static  org.junit.jupiter.api.Assertions.*;
+
+
 public class Testing {
     public static String getDayName(int Day){
         return switch(Day){
@@ -10,6 +16,22 @@ public class Testing {
             case 7 -> "Sunday";
             default -> "Invalid Day";
         };
+    }
+
+    @Test
+    void testValidDays(){
+        assertEquals("Monday", Testing.getDayName(1));
+        assertEquals("Tuesday", Testing.getDayName(2));
+        assertEquals("Wednesday", Testing.getDayName(3));
+        assertEquals("Thursday", Testing.getDayName(4));
+        assertEquals("Friday", Testing.getDayName(5));
+        assertEquals("Saturday", Testing.getDayName(6));
+        assertEquals("Sunday", Testing.getDayName(7));
+    }
+
+    @Test
+    void testInvalidDays(){
+        assertEquals("Invalid Day", Testing.getDayName(0));
     }
 
     public static void main(String[] args) {
