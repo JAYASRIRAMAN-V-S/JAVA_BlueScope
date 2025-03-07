6bd543e6c3267ce207f49a11d9071c870e5ee89e
JAYASRIRAMAN-V-S <jayasriraman2003@example.com>
2025-03-07 09:04:35 +0530

    Inheritance

--- HierarchicalInheritance.java
+++ HierarchicalInheritance.java
@@ -1,2 +1,27 @@
+class AnimalHi{
+    String name = "Lion";
+
+    void display(){
+        System.out.println(name);
+    }
+}
+
+class DogHi extends AnimalHi{
+    void dogHiDisplay(){
+        System.out.println("From the Dog Class");
+    }
+}
+
+class CheetahHi extends AnimalHi{
+    void CheetahHiDisplay(){
+        System.out.println("From the Cheetah Class");
+    }
+}
+
 public class HierarchicalInheritance {
+    public static void main(String[] args) {
+        DogHi newDog = new DogHi();
+        newDog.display();
+        newDog.dogHiDisplay();
+    }
 }
